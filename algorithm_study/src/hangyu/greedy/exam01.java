368a110203093a0dd24603a22b877fb251637e10
Ggyumalang <khg2154@naver.com>
2023-06-07 19:45:22 +0900

    20230607 commit

---
+++ algorithm_study/src/hangyu/greedy/exam01.java
@@ -0,0 +1,35 @@
+//점프를 하여 마지막 인덱스에 도달할 수 있는 지 없는 지..
+package hangyu.greedy;
+
+public class exam01 {
+
+    public static void main(String[] args) {
+        int[] nums = {2,3,1,1,4};
+        System.out.println(canJump(nums));
+
+        nums = new int[]{3,2,1,0,4};
+        System.out.println(canJump(nums));
+
+        nums = new int[]{2,0};
+        System.out.println(canJump(nums));
+    }
+
+    public static boolean canJump(int[] nums) {
+
+        if(nums[0] == 0) {
+            return false;
+        }
+        int pos = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            if(pos < i) {
+                return false;
+            } else if (pos >= nums.length-1) {
+                return true;
+            }
+
+            pos = Math.max(pos, i+nums[i]);
+        }
+
+        return false;
+    }
+}
---
+++ algorithm_study/src/hangyu/greedy/explain.md
@@ -0,0 +1,3 @@
+# 그리디 알고리즘
+# 이론 - 링크
+https://velog.io/@khg5087/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EA%B7%B8%EB%A6%AC%EB%94%94-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98
